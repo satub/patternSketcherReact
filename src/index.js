195d34d19c250c83ec5497b27553c9fed7561f2a
satub <satu.barnhill@gmail.com>
2016-11-30 11:30:24 -0500

    dummy structure still works

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 console.log("Pattern Sketcher App doesn't do much --- yet!");
 const str = 'ES6';
 console.log(`Hello ${str}`);
+import Cat from './dog'
+
+const toby = new Cat('Toby');
+
+console.log(toby.bark());
