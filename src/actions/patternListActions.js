6e11c84304d38eaae68a8450cd53ff5725c973e4
satub <satu.barnhill@gmail.com>
2016-12-01 16:52:43 -0500

    added first patternListAction

---
+++ src/actions/patternListActions.js
@@ -0,0 +1,17 @@
+const BASE_URL = 'https://hidden-oasis-19095.herokuapp.com/api/v1/'
+
+export function getPatternList(){
+  // let patterns;
+  fetch(`${BASE_URL}patterns`).then(function(res){
+    return res.json();
+  }).then(function(json){
+    return {
+      type: 'GET_PATTERN_LIST', payload: json
+    }
+  });
+}
+
+
+export function addToPatternList(){
+  console.log('Run after save and add the newest pattern to the browser, do not refetch them all?');
+}
