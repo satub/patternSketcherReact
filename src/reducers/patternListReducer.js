477c9c1746ddb3d638fcd79325940445a1e78f21
satub <satu.barnhill@gmail.com>
2016-12-02 13:20:32 -0500

    removed unneeded files

--- src/reducers/patternListReducer.js
+++
@@ -1,8 +0,0 @@
-export default function patternReducer(state = [], action) {
-  switch(action.type) {
-    case 'GET_PATTERN_LIST':
-      return action.payload.patterns;
-    default:
-    return state;
-  }
-}
