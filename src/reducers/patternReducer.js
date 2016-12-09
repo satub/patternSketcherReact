4bff0f66a239a52e42d626aa02f8fe6f364b58d5
satub <satu.barnhill@gmail.com>
2016-12-09 08:31:00 -0500

    refactored patternReducer

--- src/reducers/patternReducer.js
+++ src/reducers/patternReducer.js
@@ -11,45 +11,36 @@ const initialState = { patterns: [],
   }
 }
 export default function patternReducer(state = initialState, action) {
-  let newState;
+  let newState = JSON.parse(JSON.stringify(state));
   switch(action.type) {
     case 'GET_PATTERN':
       return state;
     case 'SAVE_PATTERN':
-      newState = JSON.parse(JSON.stringify(state));
       newState.activePattern = action.payload;
       return newState;
     case 'SAVE_PATTERN_AS_NEW':
-      newState = JSON.parse(JSON.stringify(state));
       newState.activePattern = action.payload;
       newState.patterns.push(action.payload.pattern);
       return newState;
     case 'RESET_PATTERN':
-      newState = JSON.parse(JSON.stringify(state));
       newState.activePattern = initialState.activePattern;
       return newState;
     case 'GET_PATTERN_LIST':
-      newState = JSON.parse(JSON.stringify(state));
       newState.patterns = action.payload.patterns;
       return newState;
     case 'CHOOSE_PATTERN':
-      newState = JSON.parse(JSON.stringify(state));
       newState.activePattern.pattern = action.payload.activePattern;
       return newState;
     case 'REVERSE_LOOP':
-      newState = JSON.parse(JSON.stringify(state));
       newState.activePattern.pattern = action.payload;
       return newState;
     case 'RENAME_PATTERN':
-      newState = JSON.parse(JSON.stringify(state));
       newState.activePattern.pattern = action.payload;
       return newState;
     case 'REVERSE_PATTERN':
-      newState = JSON.parse(JSON.stringify(state));
       newState.activePattern.pattern.stitches = action.payload;
       return newState;
     case 'CHANGE_SIZE':
-      newState = JSON.parse(JSON.stringify(state));
       newState.activePattern.pattern = action.payload;
       return newState;
     default:
